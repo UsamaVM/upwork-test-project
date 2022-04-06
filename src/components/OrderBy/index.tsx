96c5489d510f4498d17329fae79fbc040567028e
UsamaVM <usama.monawar@visiomate.com>
2022-04-06 16:20:08 +0500

    added sorting functionality for both random and priority, fixed some typo mistakes

--- src/components/OrderBy/index.tsx
+++ src/components/OrderBy/index.tsx
@@ -1,30 +1,32 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState } from 'react';
 
-import { OrderTypes } from "../../types/order";
-import "./index.css";
+import { OrderTypes } from '../../types/order';
+import OrderByProps from '../../types/orderby';
+import './index.css';
 
-const Job: FC = () => {
+const Job: FC<OrderByProps> = ({ toggleOrder }) => {
   const [selected, setSelected] = useState(OrderTypes.Random);
 
   const onOrderChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
     if (
       e.target.value === OrderTypes.Random ||
-      e.target.value === OrderTypes.Prioprity
+      e.target.value === OrderTypes.Priority
     ) {
       setSelected(e.target.value);
+      toggleOrder(e.target.value);
     }
   };
   return (
-    <div className="App-sortby">
-      <span className="App-sortby__label">Sort by: </span>
+    <div className='App-sortby'>
+      <span className='App-sortby__label'>Sort by: </span>
       <select
-        className="App-sortby__select"
-        id="filtersortby"
+        className='App-sortby__select'
+        id='filtersortby'
         onChange={onOrderChange}
         value={selected}
       >
         <option value={OrderTypes.Random}>Random</option>
-        <option value={OrderTypes.Prioprity}>By Priority</option>
+        <option value={OrderTypes.Priority}>By Priority</option>
       </select>
     </div>
   );
