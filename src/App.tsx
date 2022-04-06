41e5606e9c27ae5e4dc909df865f7881e3a6646c
UsamaVM <usama.monawar@visiomate.com>
2022-04-06 15:01:31 +0500

    added spinner while loading data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import Job from "./components/Job";
-import Nav from "./components/Nav";
-import OrderBy from "./components/OrderBy";
-import OrderContext from "./contexts/OrderContext";
-import { OrderTypes } from "./types/order";
+import Job from './components/Job';
+import Nav from './components/Nav';
+import OrderBy from './components/OrderBy';
+import OrderContext from './contexts/OrderContext';
+import { OrderTypes } from './types/order';
 
-import "./App.css";
+import './App.css';
 
 const App: React.FC = () => {
   const [jobs, setJobs] = useState([]);
 
   const fetchData: () => Promise<void> = async () => {
-    const result = await fetch("/jobs.json");
+    const result = await fetch('/jobs.json');
     const data = await result.json();
     setJobs(data);
   };
@@ -29,7 +29,7 @@ const App: React.FC = () => {
   });
 
   return (
-    <div className="App">
+    <div className='App'>
       <OrderContext.Provider
         value={{
           orderby: OrderTypes.Random,
@@ -37,8 +37,13 @@ const App: React.FC = () => {
         }}
       >
         <Nav />
-        {!!JobList.length && (
-          <div data-testid="app-jobs" className="App-jobs">
+        {!JobList.length ? (
+          <div className='loader-wrapper'>
+            <div className='loader'></div>
+            <span>Loading Jobs...</span>
+          </div>
+        ) : (
+          <div data-testid='app-jobs' className='App-jobs'>
             <OrderBy />
             {JobList}
           </div>
